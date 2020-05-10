1752bb6a35d410a708002ab64c1b297f8d0d06be
simonejordan03 <simonejordan03@gmail.com>
2020-05-11 00:18:23 +0200

    Prova

---
+++ CODE/System/Crypto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace OS
+{
+    internal class Crypto
+    {
+        public static void Login()
+        {
+            while (true)
+            {
+                Console.Write( "Username : " );
+                string user = Console.ReadLine();
+
+                Console.Write( "Password " );
+                Console.ForegroundColor = ConsoleColor.Black;
+                string password = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                if (password == GetPassword( "0:\\System\\users" , user ))
+                {
+                    Kernel.username = user;
+                    break;
+                }
+            }
+        }
+
+        private static string GetPassword(string path , string user)
+        {
+            string[] file = File.ReadAllLines(path);
+
+            for (int i = 0 ; i < file.Length ; i++)
+            {
+                string line = file[i];
+                if (line.Split( ":" )[0] == user)
+                {
+                    return line.Split( ":" )[2];
+                }
+            }
+
+            return "GhezziShouldChange"; //Ghezzi change this
+        }
+    }
+}
